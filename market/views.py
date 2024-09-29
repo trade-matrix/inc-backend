794732ef166e007290d1f543601f5cc9cec5a57c
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-09-29 17:14:22 +0000

    Update views.py

--- market/views.py
+++ market/views.py
@@ -700,8 +700,10 @@ class GameView(APIView):
         if not created:
             if game.today:
                 return Response({"message": "Game already initiated today"}, status=status.HTTP_400_BAD_REQUEST)
-            if wallet.balance < 10 or wallet.deposit < 10:
+            if wallet.balance < 10:
                 return Response({"message": "Insufficient funds to play game"}, status=status.HTTP_400_BAD_REQUEST)
+            if wallet.deposit < 10:
+                return Response({"message": "Insufficient deposit to play game"}, status=status.HTTP_400_BAD_REQUEST)
             if not wallet.eligible:
                 return Response({"message": "Wallet not eligible to play game"}, status=status.HTTP_400_BAD_REQUEST)
             game.today = True
