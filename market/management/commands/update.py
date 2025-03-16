c197ea7fe62a97b8ee0fc59afd23ca716f7976d7
DeAgusco <support@trade-matrix.com>
2025-03-16 15:36:08 +0000

    Update update.py

--- market/management/commands/update.py
+++ market/management/commands/update.py
@@ -1,28 +1,17 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Q
-from accounts.models import Customer
-from market.models import Transaction
+from market.models import Pool, PoolParticipant, Wallet
 
 class Command(BaseCommand):
     help = 'Updates user platforms based on email status (TM for no email, GC for with email)'
 
     def handle(self, *args, **options):
         # Update users without email to TM
-        tm_users = Customer.objects.filter(verified=True, username__startswith='bernard_')
-        #Put Gm Users in ivestment Tier 1
-        for user in tm_users:
-            customer = Customer.objects.get(username="Khalebb")
-            transaction = Transaction.objects.create(
-                    user=customer, 
-                    amount=10.00, 
-                    status='completed', 
-                    type='referal', 
-                    reffered=user.username, 
-                    image='https://darkpass.s3.us-east-005.backblazeb2.com/investment/male.png'
-                )
-            
+        all_wallets = Wallet.objects.filter(valid_for_pool=True).update(valid_for_pool=False)
+        all_pools = Pool.objects.all().update(deposits=0.00)
+        all_pool_participants = PoolParticipant.objects.all().delete()
+
         self.stdout.write(
             self.style.SUCCESS(
-                f'Successfully updated referals:\n'
+                f'Successfully updated database\n'
             )
         )
